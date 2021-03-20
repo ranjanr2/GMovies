333fa2ead2a680d2e3033758a00103380be3dc07
Rohit Ranjan <rohit.ranjan@cognizant.com>
2021-03-20 13:55:45 -0400

    @dirtiescontext for all the testcases

--- src/test/java/com/movies/Gmovies/GMoviesIT.java
+++ src/test/java/com/movies/Gmovies/GMoviesIT.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.File;
@@ -31,6 +32,7 @@ public class GMoviesIT {
 
     //POST Test
     @Test
+    @DirtiesContext
     public void createAMovieTest() throws Exception {
         GMovieDto movieDto = new GMovieDto("The Avengers","Joss Whedon","Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth",
                 "2012","Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
@@ -46,6 +48,7 @@ public class GMoviesIT {
 
     //GET Tests
     @Test
+    @DirtiesContext
     public void getAllMoviesNullTest() throws Exception {
         mockMvc.perform(get("/GMovies/Movies")
         ).andExpect(status().isOk())
@@ -54,6 +57,7 @@ public class GMoviesIT {
 
 
     @Test
+    @DirtiesContext
     public void getAllMovies() throws Exception {
         //Post the get data - Set Up
         for(int i =0 ; i< movies.size();i++)
